4af92d4ae40997011981ff740d002fc12a4ee223
Jan <jan@jan-UbuntuVirtualBox.(none)>
2013-11-10 19:00:44 +0100

    Changed to MIT License, refactored code

--- ambilight_msp430.c
+++ ambilight_msp430.c
@@ -1,43 +1,39 @@
-/* MSP430 Ambilight control code
-  Copyright (C) [2011]  [Jan Schlemminger]
-  This program is free software: you can redistribute it
-  and/or modify it under the terms of the GNU General Public License
-  as published by the Free Software Foundation, either version 3 of 
-  the License, or (at your option) any later version.
-  This program is distributed in the hope that it will be useful, 
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
-  See the GNU General Public License for more details.
-  
-  You should have received a copy of the GNU General Public License
-  along with this program. If not, see <http://www.gnu.org/licenses/>.
+/*
+Copyright (c) 2013 Jan Schlemminger
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
 #include <msp430.h>
 #include <signal.h>
 
-enum state
-{
+enum state {
   undefined,
   header,
   red,
   green,
 };
 
-enum state g_laststate = undefined;
-unsigned char g_receivedchar = 0;
+static enum state lastState = undefined;
+static unsigned char receivedChar = 0;
+const static unsigned char maxDutyCycle = 255;
 
-const unsigned int g_maxdutycycle = 255;
-
-/* prototypes */
-unsigned int calcdutycycle (unsigned int value);
-
-int
-main (void)
-{
+int main(void) {
   /* init watchdog & clock */
-  WDTCTL = WDTPW | WDTHOLD;	/* disable watchdog */
-  BCSCTL1 = CALBC1_1MHZ;	/* Set DCO */
+  WDTCTL = WDTPW | WDTHOLD; /* disable watchdog */
+  BCSCTL1 = CALBC1_1MHZ;    /* Set DCO */
   DCOCTL = CALDCO_1MHZ;
 
   /* init pin modes and selections */
@@ -60,22 +56,22 @@ main (void)
   TA0CTL |= TACLR;
   TA1CTL |= TACLR;
 
-  TA1CCR0 = g_maxdutycycle - 1;
-  TA0CCR0 = g_maxdutycycle - 1;
+  TA1CCR0 = maxDutyCycle - 1;
+  TA0CCR0 = maxDutyCycle - 1;
 
-  TA0CTL = TASSEL_2 + ID_3;	/* SMCLK, up mode */
-  TA1CTL = TASSEL_2 + ID_3;	/* SMCLK, up mode */
+  TA0CTL = TASSEL_2 + ID_3; /* SMCLK, up mode */
+  TA1CTL = TASSEL_2 + ID_3; /* SMCLK, up mode */
 
   /* init hardware UART */
-  UCA0CTL1 |= UCSSEL_2;		/* SMCLK */
-  UCA0BR0 = 104;		/* 1MHz 9600 */
-  UCA0BR1 = 0;			/* 1MHz 9600 */
-  UCA0MCTL = UCBRS0;		/* Modulation UCBRSx = 1 */
-  UCA0CTL1 &= ~UCSWRST;		/* **Initialize USCI state machine** */
-  IE2 |= UCA0RXIE;		/* Enable USCI_A0 RX interrupt */
+  UCA0CTL1 |= UCSSEL_2; /* SMCLK */
+  UCA0BR0 = 104;        /* 1MHz 9600 */
+  UCA0BR1 = 0;          /* 1MHz 9600 */
+  UCA0MCTL = UCBRS0;    /* Modulation UCBRSx = 1 */
+  UCA0CTL1 &= ~UCSWRST; /* **Initialize USCI state machine** */
+  IE2 |= UCA0RXIE;      /* Enable USCI_A0 RX interrupt */
 
   /* advanced timer settings */
-  TA1CTL |= MC_1;		/* up mode for both */
+  TA1CTL |= MC_1; /* up mode for both */
   TA0CTL |= MC_1;
 
   /* set/reset mode */
@@ -83,52 +79,32 @@ main (void)
   TA0CCTL1 = OUTMOD_3;
   TA1CCTL2 = OUTMOD_3;
 
-  __bis_SR_register (LPM0_bits + GIE);	/* Enter LPM0, interrupts enabled */
+  __bis_SR_register(LPM0_bits + GIE); /* Enter LPM0, interrupts enabled */
 
-  for (;;)
-    {
-
-    }
+  for (;;) {
+  }
 }
 
-interrupt (USCIAB0RX_VECTOR) USCI0RX_ISR ()
-{
-  g_receivedchar = UCA0RXBUF;
-  if (g_receivedchar == 0xFF && g_laststate == undefined)
-    {
-      g_laststate = header;
-      /* P1OUT ^=BIT0; */
-    }
-  else if (g_laststate != undefined)
-    {
-      switch (g_laststate)
-	{
-	case header:
-	  TA0CCR1 = calcdutycycle (g_receivedchar);	/* red */
-	  g_laststate = red;
-	  break;
-	case red:
-	  TA1CCR1 = calcdutycycle (g_receivedchar);	/* green */
-	  g_laststate = green;
-	  break;
-	case green:
-	  TA1CCR2 = calcdutycycle (g_receivedchar);	/* blue */
-	  g_laststate = undefined;
-	  break;
-	}
-    }
-}
-
-unsigned int
-calcdutycycle (unsigned int value)
-{
-  if (value < 0)
-    {
-      return 0;
-    }
-  else if (value > g_maxdutycycle)
-    {
-      return g_maxdutycycle;
-    }
-  return value;
+interrupt(USCIAB0RX_VECTOR) USCI0RX_ISR() {
+  receivedChar = UCA0RXBUF;
+  switch (lastState) {
+    case undefined:
+      if (receivedChar == 0xFF) {
+        lastState = header;
+        P1OUT ^= BIT0; /* toggle LED to indicate received header */
+      }
+      break;
+    case header:
+      TA0CCR1 = receivedChar; /* sets red PWM */
+      lastState = red;
+      break;
+    case red:
+      TA1CCR1 = receivedChar; /* sets green PWM */
+      lastState = green;
+      break;
+    case green:
+      TA1CCR2 = receivedChar; /* sets blue PWM */
+      lastState = undefined;
+      break;
+  }
 }
